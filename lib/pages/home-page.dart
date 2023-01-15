f4ade3719fd014781d43a021165b98c7b214f41c
anshyyy <anshuman9998@gmail.com>
2023-01-15 20:00:22 +0530

    created caraousel

---
+++ lib/pages/home-page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List images = ['welcome-one.png', 'welcome-two.png', 'welcome-three.png'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          itemCount: images.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('img/' + images[index]),
+                      fit: BoxFit.fill)),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
